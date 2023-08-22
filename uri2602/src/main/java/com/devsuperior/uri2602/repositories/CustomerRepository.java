1eef291a3b3a5f3ec8a0203fc194e8f2b4ecc052
felipeam10 <felipeam10@hotmail.com>
2023-08-22 14:52:52 -0300

    v4.20 - URI 2602 Spring Boot SQL

--- uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,2 +1,14 @@
-package com.devsuperior.uri2602.repositories;public interface CustomerRepository {
+package com.devsuperior.uri2602.repositories;
+
+import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT NAME FROM CUSTOMERS WHERE STATE LIKE :state")
+    List<CustomerMinProjection> search1(String state);
 }
